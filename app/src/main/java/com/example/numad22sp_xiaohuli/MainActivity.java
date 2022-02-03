75db93f482d5d283707b3e98da9cb340753f291b
XiaohuLee <li.xiaohu3@northeastern.edu>
2022-02-02 22:41:27 -0500

    A3

--- app/src/main/java/com/example/numad22sp_xiaohuli/MainActivity.java
+++ app/src/main/java/com/example/numad22sp_xiaohuli/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.numad22sp_xiaohuli;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button_am = findViewById(R.id.button_aboutMe);
+
+        button_am.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Context context = getApplicationContext();
+                CharSequence text = "Xiaohu Li li.xiaohu3@northeastern.edu";
+                int duration = Toast.LENGTH_SHORT;
+                Toast.makeText(context, text, duration).show();
+            }
+        });
+
+        final Button button_cl = findViewById(R.id.button_click);
+        button_cl.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                openMainActivity2();
+            }
+        });
+    }
+    public void openMainActivity2() {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/numad22sp_xiaohuli/MainActivity2.java
@@ -0,0 +1,68 @@
+package com.example.numad22sp_xiaohuli;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        changeText();
+    }
+
+    private void changeText() {
+        final String[] letters = {"A", "B", "C", "D", "E", "F"};
+        final TextView changeText = (TextView) findViewById(R.id.textView7);
+        Button Button_a = (Button) findViewById(R.id.button_a);
+        Button Button_b = (Button) findViewById(R.id.button_b);
+        Button Button_c = (Button) findViewById(R.id.button_c);
+        Button Button_d = (Button) findViewById(R.id.button_d);
+        Button Button_e = (Button) findViewById(R.id.button_e);
+        Button Button_f = (Button) findViewById(R.id.button_f);
+        Button_a.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                changeText.setText(letters[0]);
+            }
+        });
+        Button_b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                changeText.setText(letters[1]);
+            }
+        });
+        Button_c.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                changeText.setText(letters[2]);
+            }
+        });
+        Button_d.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                changeText.setText(letters[3]);
+            }
+        });
+        Button_e.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                changeText.setText(letters[4]);
+            }
+        });
+        Button_f.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                changeText.setText(letters[5]);
+            }
+        });
+    }
+}
